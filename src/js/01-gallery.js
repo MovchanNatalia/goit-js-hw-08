ad8f7e03a6c73c1bf54a76aa81cb7ed44dd1a85b
MovchanNatalia <Movchan.n2106@Gmail.com>
2023-04-01 19:04:07 +0300

    tasks solved

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+
+const galleryRew = document.querySelector('.gallery');
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+  )
+  .join(' ');
+
+galleryRew.insertAdjacentHTML('afterbegin', markupGallery);
+console.log(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/03-feedback.js
@@ -0,0 +1,34 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-msg';
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onEmailInput, 500));
+
+populateTextarea();
+
+function onEmailInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    return alert('Please enter your email');
+  }
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
+function populateTextarea() {
+  if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
